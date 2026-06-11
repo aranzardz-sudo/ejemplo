a3957425ec562e5da74bff1e5fe5b75eefe07162
aranzardz-sudo <arardzpin@gmail.com>
2026-06-10 18:46:22 -0600

    Update persistencia_de_datos.py

---
+++ ejemplo/persistencia_de_datos.py
@@ -0,0 +1,5 @@
+# Utilize  una lista historial = [] que almacena las operaciones realizadas
+
+#Cada vez que se presiona "=", se guarda una cadena como: "5 + 3 = 8"
+
+#El historial se pierde al cerrar la aplicación (no se guarda en disco)
